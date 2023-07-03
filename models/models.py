03ba0e5e53a863f39bd3820f05bb2b6eb55b3f6b
Shaman20 <shamansr21@gmail.com>
2023-07-03 16:34:07 +0530

    first commit

---
+++ models/models.py
@@ -0,0 +1,44 @@
+from wtforms import Form, validators, StringField, FloatField, IntegerField, DateField, SelectField
+
+# Define Add-Member-Form
+class AddMember(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    
+# Define Add-Book-Form
+class AddBook(Form):
+    id = StringField('Book ID', [validators.Length(min=1, max=11)])
+    title = StringField('Title', [validators.Length(min=2, max=255)])
+    author = StringField('Author(s)', [validators.Length(min=2, max=255)])
+    average_rating = FloatField(
+        'Average Rating', [validators.NumberRange(min=0, max=5)])
+    isbn = StringField('ISBN', [validators.Length(min=10, max=10)])
+    isbn13 = StringField('ISBN13', [validators.Length(min=13, max=13)])
+    language_code = StringField('Language', [validators.Length(min=1, max=3)])
+    num_pages = IntegerField('No. of Pages', [validators.NumberRange(min=1)])
+    ratings_count = IntegerField(
+        'No. of Ratings', [validators.NumberRange(min=0)])
+    text_reviews_count = IntegerField(
+        'No. of Text Reviews', [validators.NumberRange(min=0)])
+    publication_date = DateField(
+        'Publication Date', [validators.InputRequired()])
+    publisher = StringField('Publisher', [validators.Length(min=2, max=255)])
+    total_quantity = IntegerField(
+        'Total No. of Books', [validators.NumberRange(min=1)])
+
+
+# Define Import-Books-Form
+class ImportBooks(Form):
+    no_of_books = IntegerField('No. of Books*', [validators.NumberRange(min=1)])
+    quantity_per_book = IntegerField(
+        'Quantity Per Book*', [validators.NumberRange(min=1)])
+    title = StringField(
+        'Title', [validators.Optional(), validators.Length(min=2, max=255)])
+    author = StringField(
+        'Author(s)', [validators.Optional(), validators.Length(min=2, max=255)])
+    isbn = StringField(
+        'ISBN', [validators.Optional(), validators.Length(min=10, max=10)])
+    publisher = StringField(
+        'Publisher', [validators.Optional(), validators.Length(min=2, max=255)])
+
+
